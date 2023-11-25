87a574bbef374148615205f377df269839cf08ae
Jaklyy <102590697+Jaklyy@users.noreply.github.com>
2023-11-25 16:08:16 -0500

    add support for polygon z axis
    manually, mind you. it was painful.

--- source/main.cpp
+++ source/main.cpp
@@ -7,7 +7,7 @@
 // Misc
 constexpr int NumEntries = 66;
 constexpr u8 NumVerts = 4;
-constexpr u8 NumAxis = 2;
+constexpr u8 NumAxis = 3;
 
 // Poly Attributes
 constexpr u32 Opaque = (31 << 16);
@@ -19,9 +19,10 @@ constexpr u32 Wireframe = (0 << 16);
 
 struct Dataset
 {
-    s16 Vertices[NumVerts][NumAxis];
-    u32 PolyAttr;
-    u32 DISP3DCNT;
+    s16 Vertices[NumVerts][NumAxis]; // Coordinates of each vertex x,y,z
+    u32 PolyAttr; // Polygons attributes for each test
+    u32 DISP3DCNT; // 3d display control register bits to enable for each test
+    u8 Colors; // how many colors to read/record from the data file; 0-3 = none > r -> rg -> rgb
 };
 
 constexpr Dataset Dataset[NumEntries] =
@@ -31,51 +32,51 @@ constexpr Dataset Dataset[NumEntries] =
   // Sub Cat: Normal ----------------------------------------
 
     // Left Pos. X-Major - no fill
-    {{{-32, -32}, {32, -32}, {32, 16}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 16, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Pos. Diagonal - fill
-    {{{-32, -32}, {32, -32}, {32, 32}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 32, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Pos. Y-Major - fill
-    {{{-32, -32}, {32, -32}, {32, 48}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 48, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Neg. X-Major - fill
-    {{{-32, 32}, {32, 32}, {32, -16}, {-512, -512}},
+    {{{-32, 32, 0}, {32, 32, 0}, {32, -16, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Neg. Diagonal - fill
-    {{{-32, 32}, {32, 32}, {32, -32}, {-512, -512}},
+    {{{-32, 32, 0}, {32, 32, 0}, {32, -32, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Neg. Y-Major - fill
-    {{{-32, 32}, {32, 32}, {32, -48}, {-512, -512}},
+    {{{-32, 32, 0}, {32, 32, 0}, {32, -48, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Pos. X-Major - fill
-    {{{32, -32}, {-32, -32}, {-32, 16}, {-512, -512}},
+    {{{32, -32, 0}, {-32, -32, 0}, {-32, 16, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Pos. Diagonal - no fill
-    {{{32, -32}, {-32, -32}, {-32, 32}, {-512, -512}},
+    {{{32, -32, 0}, {-32, -32, 0}, {-32, 32, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Pos. Y-Major - no fill
-    {{{32, -32}, {-32, -32}, {-32, 48}, {-512, -512}},
+    {{{32, -32, 0}, {-32, -32, 0}, {-32, 48, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Neg. X-Major - no fill
-    {{{32, 32}, {-32, 32}, {-32, -16}, {-512, -512}},
+    {{{32, 32, 0}, {-32, 32, 0}, {-32, -16, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Neg. Diagonal - no fill
-    {{{32, 32}, {-32, 32}, {-32, -32}, {-512, -512}},
+    {{{32, 32, 0}, {-32, 32, 0}, {-32, -32, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // 12. Right Neg. Y-Major - no fill
-    {{{32, 32}, {-32, 32}, {-32, -48}, {-512, -512}},
+    {{{32, 32, 0}, {-32, 32, 0}, {-32, -48, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         0},
 
@@ -85,27 +86,27 @@ constexpr Dataset Dataset[NumEntries] =
     // Key word being generally. You might notice that vertical right slopes aren't being filled. More on that later.
 
     // Left X-Major
-    {{{0, -32}, {0, 32}, {48, 32}, {-32, -16}},
+    {{{0, -32, 0}, {0, 32, 0}, {48, 32, 0}, {-32, -16, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Y-Major
-    {{{0, -48}, {0, 32}, {16, 32}, {-16, -16}},
+    {{{0, -48, 0}, {0, 32, 0}, {16, 32, 0}, {-16, -16, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Left Diagonal
-    {{{0, -16}, {0, 32}, {16, 32}, {-16, 0}},
+    {{{0, -16, 0}, {0, 32, 0}, {16, 32, 0}, {-16, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right X-Major
-    {{{0, -32}, {-1, 32}, {-48, 32}, {32, -16}},
+    {{{0, -32, 0}, {-1, 32, 0}, {-48, 32, 0}, {32, -16, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Y-Major
-    {{{0, -48}, {-1, 32}, {-16, 32}, {16, -16}},
+    {{{0, -48, 0}, {-1, 32, 0}, {-16, 32, 0}, {16, -16, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Right Diagonal
-    {{{0, -16}, {-1, 32}, {-16, 32}, {16, 0}},
+    {{{0, -16, 0}, {-1, 32, 0}, {-16, 32, 0}, {16, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
 
@@ -114,23 +115,23 @@ constexpr Dataset Dataset[NumEntries] =
     // There are a few things that can force a polygon to fill all edges.
 
     // Transparency + Blend - Fill
-    {{{-32, -32}, {32, -32}, {32, 16}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 16, 0}, {-512, -512, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // Transparency + NO BLEND - no fill
-    {{{-32, -32}, {32, -32}, {32, 16}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 16, 0}, {-512, -512, 0}},
         Trans | POLY_CULL_NONE,
         0},
     // Wireframe - Fill
-    {{{-32, -32}, {32, -32}, {32, 16}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 16, 0}, {-512, -512, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
     // Anti-Aliasing - Fill
-    {{{-32, -32}, {32, -32}, {32, 16}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 16, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         GL_ANTIALIAS},
     // Edge Marking - Fill
-    {{{-32, -32}, {32, -32}, {32, 16}, {-512, -512}},
+    {{{-32, -32, 0}, {32, -32, 0}, {32, 16, 0}, {-512, -512, 0}},
         Opaque | POLY_CULL_NONE,
         GL_OUTLINE},
 
@@ -143,27 +144,27 @@ constexpr Dataset Dataset[NumEntries] =
     // out with all the other weird behavior this gpu has thrown out.
 
     // simple line polygon - fill
-    {{{-10, -5}, {-10, -5}, {10, 5}, {-512, 0}},
+    {{{-10, -5, 0}, {-10, -5, 0}, {10, 5, 0}, {-512, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // not quite a line polygon - no fill
-    {{{-11, -6}, {-10, -5}, {10, 5}, {-512, 0}},
+    {{{-11, -6, 0}, {-10, -5, 0}, {10, 5, 0}, {-512, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // if you can see this you're doing something wrong - no fill
-    {{{-2, -1}, {0, 0}, {2, 1}, {-512, 0}},
+    {{{-2, -1, 0}, {0, 0, 0}, {2, 1, 0}, {-512, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // cursed line polygon 1 - fill, but like, only the line part
-    {{{47, 8}, {-46, -27}, {47, 27}, {-46, -27}},
+    {{{47, 8, 0}, {-46, -27, 0}, {47, 27, 0}, {-46, -27, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // cursed line polygon 2 - see above
-    {{{47, 8}, {-46, -27}, {0, 0}, {-46, -27}},
+    {{{47, 8, 0}, {-46, -27, 0}, {0, 0, 0}, {-46, -27, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // cursed line polygon 3 - fill
-    {{{48, 0}, {0, -27}, {-40, -8}, {0, -27}},
+    {{{48, 0, 0}, {0, -27, 0}, {-40, -8, 0}, {0, -27, 0}},
         Opaque | POLY_CULL_NONE,
         0},
 
@@ -174,38 +175,38 @@ constexpr Dataset Dataset[NumEntries] =
     // oops
 
     // half filled. oh dear.
-    {{{-110, -12}, {-110, 24}, {0, 24}, {-140, 0}},
+    {{{-110, -12, 0}, {-110, 24, 0}, {0, 24, 0}, {-140, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // is the slope half filled or half unfilled?
-    {{{50, -30}, {-30, 50}, {0, 50}, {0, -20}},
+    {{{50, -30, 0}, {-30, 50, 0}, {0, 50, 0}, {0, -20, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // x major slopes are never filled by this glitch, though. - no fill
-    {{{50, -30}, {-20, 20}, {0, 20}, {0, -20}},
+    {{{50, -30, 0}, {-20, 20, 0}, {0, 20, 0}, {0, -20, 0}},
         Opaque | POLY_CULL_NONE,
         0},
 
   // Sub Cat: Trapezoids ------------------------------------
 
     // this is a fill rule ...for some reason?
-    {{{-25, 10}, {25, 10}, {50, -10}, {-50, -10}},
+    {{{-25, 10, 0}, {25, 10, 0}, {50, -10, 0}, {-50, -10, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // it applies to clipped vertices too!
-    {{{-50, 90}, {50, 90}, {0, 110}, {-512, 0}},
+    {{{-50, 90, 0}, {50, 90, 0}, {0, 110, 0}, {-512, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // only x major slopes though
-    {{{-25, 10}, {25, 10}, {35, -10}, {-45, -10}},
+    {{{-25, 10, 0}, {25, 10, 0}, {35, -10, 0}, {-45, -10, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // does not work when both the bottom of both slopes have the same x coord
-    {{{0, 10}, {0, 10}, {50, -10}, {-50, -10}},
+    {{{0, 10, 0}, {0, 10, 0}, {50, -10, 0}, {-50, -10, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // even when their y coord is different
-    {{{0, 5}, {0, 10}, {50, -10}, {-50, -10}},
+    {{{0, 5, 0}, {0, 10, 0}, {50, -10, 0}, {-50, -10, 0}},
         Opaque | POLY_CULL_NONE,
         0},
 
@@ -214,36 +215,36 @@ constexpr Dataset Dataset[NumEntries] =
     // vertical right edges are shifted left by one pixel
 
     // square
-    {{{-32, -32}, {-32, 32}, {32, 32}, {32, -32}},
+    {{{-32, -32, 0}, {-32, 32, 0}, {32, 32, 0}, {32, -32, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // vertical 0 wide line polygon, right edges are *not* shifted left
-    {{{0, -32}, {0, 32}, {0, 32}, {0, -32}},
+    {{{0, -32, 0}, {0, 32, 0}, {0, 32, 0}, {0, -32, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // vertical 1 wide line polygon, right edges are shifted left
-    {{{0, -32}, {0, 32}, {1, 32}, {1, -32}},
+    {{{0, -32, 0}, {0, 32, 0}, {1, 32, 0}, {1, -32, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // but not when touching the edge of the screen
     // todo: test if it is shifted left when drawn oob? cause im pretty sure it does matter
-    {{{-128, -32}, {-128, 32}, {-127, 32}, {-127, -32}},
+    {{{-128, -32, 0}, {-128, 32, 0}, {-127, 32, 0}, {-127, -32, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // swapped polygon 1
-    {{{-32, -32}, {0, -32}, {0, 32}, {32, 32}},
+    {{{-32, -32, 0}, {0, -32, 0}, {0, 32, 0}, {32, 32, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // swapped polygon 1 reversed
-    {{{32, -32}, {0, -32}, {0, 32}, {-32, 32}},
+    {{{32, -32, 0}, {0, -32, 0}, {0, 32, 0}, {-32, 32, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // idk what this one even is
-    {{{-32, -32}, {-32, 32}, {32, 0}, {-32, 0}},
+    {{{-32, -32, 0}, {-32, 32, 0}, {32, 0, 0}, {-32, 0, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
     // still dont know
-    {{{32, -32}, {32, 32}, {-32, 0}, {32, 0}},
+    {{{32, -32, 0}, {32, 32, 0}, {-32, 0, 0}, {32, 0, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
 
@@ -255,15 +256,15 @@ constexpr Dataset Dataset[NumEntries] =
     // ...even though vertical edges shouldn't be
 
     // swapped polygons are very glitchy, it turns out
-    {{{-32, -32}, {0, -32}, {0, 32}, {32, 32}},
+    {{{-32, -32, 0}, {0, -32, 0}, {0, 32, 0}, {32, 32, 0}},
         Opaque | POLY_CULL_NONE,
         GL_ANTIALIAS},
     // see above
-    {{{32, -32}, {0, -32}, {0, 32}, {-32, 32}},
+    {{{32, -32, 0}, {0, -32, 0}, {0, 32, 0}, {-32, 32, 0}},
         Opaque | POLY_CULL_NONE,
         GL_ANTIALIAS},
     // edge case within an edge case combined with an edge case
-    {{{-110, -12}, {-110, 24}, {0, 24}, {-140, 0}},
+    {{{-110, -12, 0}, {-110, 24, 0}, {0, 24, 0}, {-140, 0, 0}},
         Opaque | POLY_CULL_NONE,
         GL_ANTIALIAS},
 
@@ -276,39 +277,39 @@ constexpr Dataset Dataset[NumEntries] =
     // todo2: also need to test if this applies with z clipping as well... in theory it should...?
 
     // Horizontal line polygons always render 1-4,
-    {{{-32, 0}, {0, 0}, {48, 0}, {32, 0}},
+    {{{-32, 0, 0}, {0, 0, 0}, {48, 0, 0}, {32, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // 1-2,
-    {{{-32, 0}, {32, 0}, {48, 0}, {0, 0}},
+    {{{-32, 0, 0}, {32, 0, 0}, {48, 0, 0}, {0, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // or 2-4. 3 is never rendered
-    {{{0, 0}, {-32, 0}, {48, 0}, {32, 0}},
+    {{{0, 0, 0}, {-32, 0, 0}, {48, 0, 0}, {32, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Until the first vertex gets clipped.... (1-2)
-    {{{-128, 0}, {-32, 0}, {32, 0}, {48, 0}},
+    {{{-128, 0, 0}, {-32, 0, 0}, {32, 0, 0}, {48, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // Then things start to get interesting... (2-3)
-    {{{-128, 0}, {-128, 0}, {32, 0}, {48, 0}},
+    {{{-128, 0, 0}, {-128, 0, 0}, {32, 0, 0}, {48, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // 2-3
-    {{{-128, 0}, {-128, 0}, {32, 0}, {48, 0}},
+    {{{-128, 0, 0}, {-128, 0, 0}, {32, 0, 0}, {48, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // 3-4
-    {{{-128, 0}, {-128, 0}, {-128, 0}, {48, 0}},
+    {{{-128, 0, 0}, {-128, 0, 0}, {-128, 0, 0}, {48, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // 4th vertex being clipped might complicate things, might not, probably depends on implementation of clipping (1-2)
-    {{{-128, 0}, {-32, 0}, {32, 0}, {-128, 0}},
+    {{{-128, 0, 0}, {-32, 0, 0}, {32, 0, 0}, {-128, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     //  2-3
-    {{{-128, 0}, {-128, 0}, {32, 0}, {-128, 0}},
+    {{{-128, 0, 0}, {-128, 0, 0}, {32, 0, 0}, {-128, 0, 0}},
         Opaque | POLY_CULL_NONE,
         0},
         
@@ -316,15 +317,15 @@ constexpr Dataset Dataset[NumEntries] =
  
     // left side has precedence over right side
     // Just an innocent square c:
-    {{{-32, -32}, {-32, 32}, {32, 33}, {32, -32}},
+    {{{-32, -32, 0}, {-32, 32, 0}, {32, 33, 0}, {32, -32, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     // pointer never goes backwards
-    {{{-30, -14}, {20, 15}, {52, 15}, {-51, 10}},
+    {{{-30, -14, 0}, {20, 15, 0}, {52, 15, 0}, {-51, 10, 0}},
         Opaque | POLY_CULL_NONE,
         0},
     //these slopes are identical, but render differently due to rounding.
-    {{{-64,0}, {64, -41}, {64, 41}, {-512, 0}},
+    {{{-64,0, 0}, {64, -41, 0}, {64, 41, 0}, {-512, 0, 0}},
         Trans | POLY_CULL_NONE,
         GL_BLEND},
 
@@ -333,27 +334,27 @@ constexpr Dataset Dataset[NumEntries] =
     // Second vertex behaves differently vs others for w/e reason.
 
     // should behave normally
-    {{{-32, -32}, {-1, -1}, {32, 32}, {32, -31}},
+    {{{-32, -32, 0}, {-1, -1, 0}, {32, 32, 0}, {32, -31, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
     // should behave normally ...or not?
-    {{{-32, -32}, {0, 0}, {32, 32}, {32, -31}},
+    {{{-32, -32, 0}, {0, 0, 0}, {32, 32, 0}, {32, -31, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
     // should glitch out, behaving as if it was swapped
-    {{{-32, -32}, {1, -1}, {32, 32}, {32, -31}},
+    {{{-32, -32, 0}, {1, -1, 0}, {32, 32, 0}, {32, -31, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
     // however only the second vertex should cause this behavior
-    {{{1, -1}, {32, 32}, {32, -32}, {-32, -31}},
+    {{{1, -1, 0}, {32, 32, 0}, {32, -32, 0}, {-32, -31, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
     // should also behave normally
-    {{{-32, -32}, {-32, -32}, {32, 32}, {32, -31}},
+    {{{-32, -32, 0}, {-32, -32, 0}, {32, 32, 0}, {32, -31, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
     // not sure if this one would behave weirdly tbh? idk im just gonna throw it in for the heck of it. might get removed.
-    {{{-2, -1}, {0, 0}, {-2, -1}, {2, 1}},
+    {{{-2, -1, 0}, {0, 0, 0}, {-2, -1, 0}, {2, 1, 0}},
         Wireframe | POLY_CULL_NONE,
         0},
 };
@@ -562,7 +563,7 @@ void drawPoly(int iteration)
     for (int i = 0; i < verts; i++)
     {
         glColor3b(255, 255, 255);
-        glVertex3v16(Dataset[iteration].Vertices[i][0], Dataset[iteration].Vertices[i][1], 0);
+        glVertex3v16(Dataset[iteration].Vertices[i][0], Dataset[iteration].Vertices[i][1], Dataset[iteration].Vertices[i][2]);
     }
     glEnd();
         
